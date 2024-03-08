829a5ac1e1ecc8150f0c4cfb98963c1ee8601306
Edoardo Esposito <relaxededo00@gmail.com>
2024-03-08 12:33:57 +0100

    new

---
+++ users.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const users = "Luigi De Russis, Luca Mannella, Fulvio Corno, Juan Pablo Saenz Moreno, Luca Pezzolla";
+const userArray = users.split(",");
+for(let i=0;i<userArray.length;i++){
+    userArray[i] = userArray[i].trim();
+}
+console.log(userArray);
+
+const acronyms=[];
+for(const name of userArray){
+    let acronym = name[0];
+    for(let i=1;i<name.length;i++){
+        if(name[i]== ' ' && name[i+1]!= ' '){
+            acronym = acronym + name[i+1];
+        }
+    }
+    acronyms.push(acronym.toUpperCase());
+}
+
+console.log(acronyms);
