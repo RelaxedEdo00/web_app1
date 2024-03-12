729270940b7c9c5edf5678492aedc978ce5dd0dd
Edoardo Esposito <relaxededo00@gmail.com>
2024-03-12 13:00:56 +0100

    fine lezione

--- week2/query.mjs
+++ week2/query.mjs
@@ -1,5 +1,6 @@
 // with mjs we don't have to write use strict
 import 'dayjs';
+import dayjs from 'dayjs';
 
 function Question(text, user, date){
     this.text = text;
@@ -8,7 +9,16 @@ function Question(text, user, date){
     this.answers = [];
 
     this.add = (answer) => {this.answers.push(answer);}
-
+    this.find = (user) => this.answers.filter(a => a.user === user);
+    this.afterDate = (date) => this.answers.filter(a => (a.date.isAfter(date) || a.date.isSame(date)));
+    this.listByDate = () => {
+        const ans =[...this.answers];
+        ans.sort((a1,a2) => (a1.date.valueOf()-a2.date.valueOf()));
+    }
+    this.listByScore =  () => {
+        const ans = [...this.answers];
+        ans.sort((a1,a2)=>a1.score - a2.score);
+    }
 }
 
 function Answer(response, user, score, date){
@@ -22,13 +32,15 @@ function Answer(response, user, score, date){
     this.getCategory =  () => {return category;}
 }
 
-const a1 = new Answer('Yes', 'fulvio', 5, '2024-03-12');
-console.log(a1);
-a1.voteUp();
-console.log(a1);
-console.log(a1.getCategory());
+const d = dayjs();
+console.log(d.format());
 
-const q1 = new Question('Are you happy?', 'fulvio', '2024-03-12');
-console.log(q1);
+const a1 = new Answer('Yes', 'fulvio', 5, dayjs('2024-03-12'));
+a1.voteUp();
+
+const q1 = new Question('Are you happy?', 'fulvio', dayjs('2024-03-12'));
 q1.add(a1);
-console.log(q1);
+
+q1.add(new Answer('Maybe', 'luigi', 0, dayjs()));
+q1.add(new Answer('No', 'fulvio', -2, dayjs('2024-03-12')));
+q1.add(new Answer('Undecided', 'luigi', 0, dayjs()));
